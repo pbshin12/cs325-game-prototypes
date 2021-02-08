7c81caeb1dbbefcf19d912942250e2b55028afb7
Peter Shin <pshin2@masonlive.gmu.edu>
2021-02-08 00:53:06 -0500

    A lot of stuff happened
    - Added gun shot
    - Added fart
    - Added sound ambience
    - Attempted timer (failed so far)
    - Attempted random int (failed so far)

--- DigitalAssignment1/js/main.js
+++ DigitalAssignment1/js/main.js
@@ -16,42 +16,99 @@ class MyScene extends Phaser.Scene {
     constructor() {
         super();
         
-        this.bouncy = null;
+        this.bg = null;
     }
     
     preload() {
         // Load an image and call it 'logo'.
-        this.load.image( 'logo', 'assets/phaser.png' );
+        this.load.image( 'bg', 'assets/8bit_Desert.png' );
+		
+		// Loading audio
+		/* https://phaser.io/examples/v3/view/audio/web-audio/play-sound-on-keypress */
+		this.load.audio('gunShot', ['assets/Audio/GunShotSoundEffect.ogg', 'assets/Audio/GunshotSoundEffect.mp3']);
+		this.load.audio('reload', ['assets/Audio/pickUp.ogg', 'assets/Audio/pickUp.mp3']);
+		this.load.audio('wind', ['assets/Audio/DesertWindSound.ogg', 'assets/Audio/DesertWindSound.mp3']);
+		this.load.audio('fart', ['assets/Audio/dry-fart.ogg', 'assets/Audio/dry-fart.mp3']);
+		
     }
     
+	
     create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        this.bouncy = this.physics.add.sprite( this.cameras.main.centerX, this.cameras.main.centerX, 'logo' );
         
-        // Make it bounce off of the world bounds.
-        this.bouncy.body.collideWorldBounds = true;
-        
-        // Make the camera shake when clicking/tapping on it.
-        this.bouncy.setInteractive();
-        this.bouncy.on( 'pointerdown', function( pointer ) {
-            this.scene.cameras.main.shake(500);
-            });
-        
+		// Adds image to the center of the screen
+		// Below is a link to how to correctly scale the image to the screen
+		/* https://phaser.discourse.group/t/how-to-stretch-background-image-on-full-screen/1839 */
+		this.bg = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'bg');
+		let scaleX = this.cameras.main.width / this.bg.width;
+		let scaleY = this.cameras.main.height / this.bg.height;
+		let scale = Math.max(scaleX, scaleY);
+		this.bg.setScale(scale).setScrollFactor(0);
+		
+		// Listens to keyboard events
+		/* https://rexrainbow.github.io/phaser3-rex-notes/docs/site/keyboardevents/ */
+		this.keyObj = this.input.keyboard.addKey('SPACE');
+		this.isDown = this.keyObj.isDown;
+		this.isUp = this.keyObj.isUp;
+		this.shotFired = false; // Ensures that only 1 shot is fired
+		
+		// Sound
+		/* https://www.youtube.com/watch?v=COncYQLGJS8 */
+		this.gunShot = this.sound.add('gunShot');
+		this.reload = this.sound.add('reload');
+		this.wind = this.sound.add('wind');
+		this.fart = this.sound.add('fart');
+		
+		// Should play when the game starts
+		this.reload.play();
+		this.wind.play();
+		this.fart.play();
+		
+		// Obtain random integer
+		//this.randomInt = getRandomInt(20);
+		
+		// Timer (with random time)
+		/* https://phaser.io/examples/v2/time/basic-timed-event */
+		//this.time = this.time.events.add(Phaser.Timer.SECOND * this.randomInt, fart, this);
+		
+
+		
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
         let style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        let text = this.add.text( this.cameras.main.centerX, 15, "Digital Assignment 1 Prototype", style );
+        let text = this.add.text( this.cameras.main.centerX, 15, "Digital Assignment 1 Reflex Prototype", style );
         text.setOrigin( 0.5, 0.0 );
     }
     
     update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        this.bouncy.rotation = this.physics.accelerateToObject( this.bouncy, this.input.activePointer, -500, 500, 500 );
+		
+		//TODO --> Hidden timer & sound effect to react
+		
+		// Shooting
+		if (this.keyObj.isDown && !(this.shotFired)) {
+			this.gunShot.play();
+			this.shotFired = true;
+		}
+		
     }
+	
+	/*
+	// Resets the game
+	reset() {
+		this.shotsFired = false;
+	}
+	
+	// Obtains a random integer between a range from 0 to a max value (inclusive)
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+	getRandomInt(max) {
+		return Math.floor(Math.random() * Math.floor(max));
+	}
+	
+	// Alerts the player to react (yes... it's a fart sound effect)
+	fart() {
+		this.fart.play();
+	}*/
+
+
 }
 
 const game = new Phaser.Game({
