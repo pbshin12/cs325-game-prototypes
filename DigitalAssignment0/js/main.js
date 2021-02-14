f06121bd46ec813ba899eba784a90b25e3316fb4
Peter Shin <pshin2@masonlive.gmu.edu>
2021-02-14 06:29:16 -0500

    Added Flappy Bird
    Was able to successfully upload Unity Games :)

--- DigitalAssignment0/js/main.js
+++ DigitalAssignment0/js/main.js
@@ -21,7 +21,7 @@ class MyScene extends Phaser.Scene {
     
     preload() {
         // Load an image and call it 'logo'.
-        this.load.image( 'logo', 'assets/large.jpg' );
+        this.load.image( 'logo', 'assets/large.png' );
     }
     
     create() {
