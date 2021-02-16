d2023d02fde59032eb033ea81766c853700994db
Peter Shin <pshin2@masonlive.gmu.edu>
2021-02-15 23:34:11 -0500

    game finished

--- DigitalAssignment2/js/main.js
+++ DigitalAssignment2/js/main.js
@@ -14,7 +14,7 @@ import "./phaser.js";
 class MyScene extends Phaser.Scene {
 
     constructor() {
-        super();
+        super({ key: 'MyScene' })
         this.mouse;
     }
     
@@ -60,15 +60,19 @@ class MyScene extends Phaser.Scene {
         this.weaponSprite = this.add.image(0, 0, 'weapon').setOrigin(0, 0.5);
         this.weaponSprite.setScale(0.05);
 
-        this.squirrels = this.physics.add.group();
-        this.squirrels.create(100, 100, 'squirrel');
+        //this.squirrels = this.physics.add.group();
+        this.squirrels = this.physics.add.image(-10, -50, 'squirrel');
+        this.all_squirrels = [];
+        this.all_squirrels.push(this.squirrels);
         this.done = false;
 
         // Both the player and the weapon sprite are now grouped
         /* https://phaser.io/examples/v3/view/game-objects/container/arcade-physics-body-test */
         // Container will group sprites together
-        this.bullet = this.physics.add.image(0, 0, 'bullet');
-        this.container = this.add.container(400, 300, [this.playerSprite, this.weaponSprite]);
+        this.bullet = this.physics.add.image(800, 800, 'bullet');
+        this.allBullets = [];
+        this.allBullets.push(this.bullet);
+        this.container = this.add.container(400, 500, [this.playerSprite, this.weaponSprite]);
         this.container.setSize(64, 64);
         this.container.setScrollFactor(0);
         this.physics.world.enable(this.container);
@@ -84,6 +88,7 @@ class MyScene extends Phaser.Scene {
         this.S = this.input.keyboard.addKey('S');
         this.D = this.input.keyboard.addKey('D');
         this.R = this.input.keyboard.addKey('R');
+        this.SPACE = this.input.keyboard.addKey('SPACE');
         this.mouse = this.input;
         this.leftClick = this.input.mousePointer;
         
@@ -95,7 +100,7 @@ class MyScene extends Phaser.Scene {
 
         /* https://codepen.io/samme/pen/JjPzaVb?editors=0010 */
         this.angleUpdate;
-        this.text = this.add.text(20, 40, '', { font: '16px monospace', fill: 'violet' }).setScrollFactor(0);
+        this.text = this.add.text(20, 30, '', { font: '16px monospace', fill: 'violet' }).setScrollFactor(0);
         this.input.on('pointermove', function (pointer) {
             let angle = Phaser.Math.Angle.Between(this.container.x, this.container.y, pointer.worldX, pointer.worldY);
             this.angleUpdate = angle;
@@ -110,99 +115,181 @@ class MyScene extends Phaser.Scene {
             else {
                 this.weaponSprite.flipY = false;
             }
-            // Debugging
-            this.text.setText([
-              `angle:      ${angle.toFixed(2)} rad  ${Phaser.Math.RadToDeg(angle).toFixed(2)} deg`,
-              `mouse: x --> ${this.mouse.mousePointer.worldX} y --> ${this.mouse.mousePointer.worldY}`,
-              `reload timer: ${this.control}`
-            ]);
+            
           }, this);
   
         this.cameras.main.startFollow(this.container, true, 0.9, 0.9);
 
         this.control = 0; // Controlling the rate of fire on the gun
-        this.firedOnce = false;
+        this.spawnRate = 250; // Controls the spawn rate of the squirres (the code's looking really messy, I'll do better next time)
+        this.spawnRateCounter = 0;
+        this.spawnCounter = 0;
+        this.squirrelSpeed = 100;
+        this.squirrelsKilled = 0;
+        this.difficultyTimer = 0;
+        this.playerAlive = true;
+        this.start = false;
+
+        this.fired = false;
         this.worldBounds = this.physics.world.bounds;
 
-        let tutorialStyle = { font: "30px Verdana", fill: "#000000", align: "center" };
-		let text = this.add.text( this.cameras.main.centerX, 500, "\"WASD\" to Move\t\"Left-Click\" to fire\t\"R\" to reset", tutorialStyle );
-		text.setOrigin( 0.5, 0.0 );
+        let tutorialStyle = { font: "15px Verdana", fill: "#000000", align: "center" };
+        this.announcementStyle = { font: "30px Verdana", fill: "#FFFFFF", align: "center" };
+        this.deathStyle = { font: "20px Verdana", fill: "#FFFFFF", align: "center" };
+		let text = this.add.text( this.cameras.main.centerX + 270, 550, "\"A or D\" to Move\n\"Left-Click\" to fire\t\"R\" to reset", tutorialStyle );
+        text.setOrigin(0.5, 0);
+        this.announcementText = null;
+        
+        this.announcementText = this.add.text( this.cameras.main.centerX, this.cameras.main.centerY, "Presss \"SPACE\" to Start\nDon't get run over by them squirrels", this.announcementStyle );
+        this.announcementText.setOrigin(0.5, 0);
+
+
+        
+        
     }
     
     update() {
         let i = 0;
-        while (i <= 7) {
-            if (this.done === true) {break;}
-            let randX = Phaser.Math.Between(10, 780);
-            this.squirrels.create(randX, 0, 'squirrel');
-            i++;
-        }
-        this.done = true;
-        
+        let randX = Phaser.Math.Between(-10, 780);
+        let randXOffSet = Phaser.Math.Between(200, 500);
 
-        this.container.body.setVelocity(0, 0);
-        // Player movement --> Moves in all 8 directions
-        if (this.A.isDown) {
-            this.container.body.setVelocity(-300, 0);
-            if (this.S.isDown) {
-                this.container.body.setVelocity(-300, 300);
-            }
-            else if (this.W.isDown) {
-                this.container.body.setVelocity(-300, -300);
-            }
-        }
-        else if (this.D.isDown) {
-            this.container.body.setVelocity(300, 0);
-            if (this.S.isDown) {
-                this.container.body.setVelocity(300, 300);
-            }
-            else if (this.W.isDown) {
-                this.container.body.setVelocity(300, -300);
-            }
-        }
-        else if (this.S.isDown) {
-            this.container.body.setVelocity(0, 300);
-        }
-        else if (this.W.isDown) {
-            this.container.body.setVelocity(0, -300);
-        }
-        else {
-            this.container.body.setVelocity(0, 0);
+        if (!this.playerAlive) {
+            this.announcementText = this.add.text( this.cameras.main.centerX, this.cameras.main.centerY - 25, 
+                `You got ran over and died a miserable death\nThe squirrels took over the world\nand became the dominant species on Earth\n\nSquirrels Killed: ${this.squirrelsKilled}`, this.deathStyle );
+            this.announcementText.setOrigin(0.5, 0);
         }
 
-        if(this.W.isUp && this.A.isUp && this.S.isUp && this.D.isUp) {
-            this.container.body.setVelocity(0, 0);
+        if (this.SPACE.isDown && this.playerAlive) {
+            this.start = true;
+            this.announcementText.destroy();
         }
 
-        if (!(this.firedOnce) && this.leftClick.isDown) {
-            this.fire(this.angleUpdate);
-            this.firedOnce = true;
-        }
 
-        if (this.leftClick.isDown && this.control >= 50) {
-            this.fire(this.angleUpdate);
+        if (this.playerAlive && this.start) {
+
+            if (this.spawnRateCounter >= this.spawnRate) {
+                this.squirrels = this.physics.add.image(randX + randXOffSet, 0, 'squirrel');
+                this.all_squirrels.push(this.squirrels);
+                this.squirrels = this.physics.add.image(randX, 0, 'squirrel');
+                this.all_squirrels.push(this.squirrels);
+                this.spawnRateCounter = 0;
+            }
+            else {
+                this.spawnRateCounter++;
+            }
+
+            this.difficultyTimer++;
+
+            // Increase difficulty every 500 ticks
+            if (this.difficultyTimer >= 500) {
+                console.log("Difficulty increased");
+                this.squirrelSpeed += 30;
+                this.spawnRate -= 25
+
+                // Lowest spawnRate cap
+                if (this.spawnRate < 50) {
+                    this.spawnRate = 40;
+                }
+
+                this.difficultyTimer = 0;
+
+            }
+
+            // This is an expensive operation, but this'll do
+            for (i = 0; i < this.all_squirrels.length; i++) {
+
+                this.physics.moveToObject(this.all_squirrels[i], this.container, this.squirrelSpeed);
+                this.physics.add.overlap(this.all_squirrels[i], this.container, this.killPlayer, null, this);
+                this.physics.add.overlap(this.bullet, this.all_squirrels[i], this.destroy, null, this);            
+            }
+
+
+            for (i = 0; i < this.allBullets.length; i++) {
+                if (this.allBullets[i].x <= 0 || this.allBullets[i].x >= 800 || this.allBullets[i].y <= 0 || this.allBullets[i] >= 600) {
+                    this.allBullets[i].disableBody(true, true);
+                    this.allBullets.splice(i, 1);
+                    break;
+                }
+            }
+            
+            
+            
+
+            //this.container.body.setVelocity(0, 0);
+            // Player movement --> Moves in all 8 directions --> Update: Now moves in 2 directions: left and right
+            if (this.A.isDown) {
+                //this.container.body.setVelocity(-300, 0);
+                this.container.x -= 3;
+                // if (this.S.isDown) {
+                //     this.container.body.setVelocity(-300, 300);
+                // }
+                // else if (this.W.isDown) {
+                //     this.container.body.setVelocity(-300, -300);
+                // }
+            }
+            if (this.D.isDown) {
+                //this.container.body.setVelocity(300, 0);
+                this.container.x += 3;
+                // if (this.S.isDown) {
+                //     this.container.body.setVelocity(300, 300);
+                // }
+                // else if (this.W.isDown) {
+                //     this.container.body.setVelocity(300, -300);
+                // }
+            }
+            // else if (this.S.isDown) {
+            //     this.container.body.setVelocity(0, 300);
+            // }
+            // else if (this.W.isDown) {
+            //     this.container.body.setVelocity(0, -300);
+            // }
+            // else {
+            //     this.container.body.setVelocity(0, 0);
+            // }
+
+            // if(this.W.isUp && this.A.isUp && this.S.isUp && this.D.isUp) {
+            //     this.container.body.setVelocity(0, 0);
+            // }
+
+            // if (!(this.firedOnce) && this.leftClick.isDown) {
+            //     this.fire(this.angleUpdate);
+            //     this.firedOnce = true;
+            // }
+
+            // if (this.leftClick.isDown && this.control >= 50) {
+            //     this.fire(this.angleUpdate);
+            // }
+            
+            // this.control++;
+
+            if (this.leftClick.isDown && !(this.fired)) {
+                this.fire(this.angleUpdate);
+                this.fired = true;
+            }
+            if (this.fired && !(this.leftClick.isDown)) {
+                this.fired = false;
+            }
         }
-        this.physics.add.overlap(this.bullet, this.squirrels, this.destroy, null, this);
-        this.control++;
-        
 
         if (this.R.isDown) {
             this.reset();
         }
         
-
+        // Debugging
+        this.text.setText([
+            `spawnrate counter: ${this.spawnRateCounter}`,
+            `Squirrel Speed: ${this.squirrelSpeed}`,
+            `Squirrel SpawnRate: ${this.spawnRate}`,
+            `Difficulty timer: ${this.difficultyTimer}`
+          ]);
     }
 
     fire(angle) {
-        console.log("Firing");
-        //let localPoint = this.getLocalPoint()
-        //this.bullet = this.physics.add.sprite(this.weaponSprite.x, this.weaponSprite.y, 'bullet');
         this.gunShot.play();
         this.bullet = this.physics.add.sprite(this.container.x, this.container.y, 'bullet').copyPosition(this.container).setScale(0.05);
         this.bullet.rotation = angle;
-
-
-        this.physics.moveToObject(this.bullet, this.crossHair, 800);
+        this.allBullets.push(this.bullet);
+        this.physics.moveToObject(this.bullet, this.crossHair, 1000);
         this.control = 0;
         
 
@@ -215,6 +302,18 @@ class MyScene extends Phaser.Scene {
         this.squeak.play();
         squirrel.disableBody(true, true);
         bullet.disableBody(true, true);
+
+        let s_Index = this.all_squirrels.indexOf(squirrel);
+        let b_Index = this.allBullets.indexOf(bullet);
+
+        this.all_squirrels.splice(s_Index, 1);
+        this.allBullets.splice(b_Index, 1);
+
+        this.squirrelsKilled++;
+    }
+
+    killPlayer(squirrel, container) {
+        this.playerAlive = false;
     }
 
     reset() {
@@ -231,7 +330,7 @@ const game = new Phaser.Game({
     pixelArt: true, // Documentation states that setting this in the configs will make pixel art look clear (and it works!)
     physics: { default: 'arcade',
                 arcade: {
-                    debug: true
+                    debug: false
                 } 
             },
     });
