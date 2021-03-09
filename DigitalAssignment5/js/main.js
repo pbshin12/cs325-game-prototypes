d9c8d170f103203ada1d579b356daa3b87b94d3d
Peter Shin <pshin2@masonlive.gmu.edu>
2021-03-09 01:33:07 -0500

    Finished game
    Known bugs:
    - hitbox is extremely wack, Unity can fix this VERY easily, but I don't know how Phaser works
    - Controls are also wack, multiple inputs will lead the player character to permanently move to a single direction

--- DigitalAssignment5/js/main.js
+++ DigitalAssignment5/js/main.js
@@ -37,42 +37,52 @@ class MyScene extends Phaser.Scene {
         this.physics.world.setBounds(-30, 0, 860, 600);
         this.playerAlive = true; // Checks if player is alive
 
+        this.lives = 3;
         this.score = 0;
         this.captured = 0;
-        this.dogSpawnRate = 10000; // 10 seconds to spawn a dog
+        this.dogSpawnRate = 15000; // 15 seconds to spawn a dog
+        this.trashSpawnRate = 3000; // 3 seconds to spawn trash
         
         // Box Position
         this.boxPosition = 2;
         this.boxIsUp = false;
 
-        this.gameStart = false;
+        this.gameStart = true;
         this.scoreText = this.add.text(16, 16, `score: ${this.score}`, {fontSize: '32px', fill: '#FFFFFF'});
-        this.capturedText = this.add.text(16, 64, `captured: ${this.score}`, {fontSize: '32px', fill: '#FFFFFF'});
+        this.capturedText = this.add.text(16, 64, `captured: ${this.captured}`, {fontSize: '32px', fill: '#FFFFFF'});
+        this.healthText = this.add.text(16, 120, `HEALTH: ${this.lives}`, {fontSize: '32px', fill: '#FFFFFF'});
 
-        // timer event - https://phaser.io/examples/v3/view/time/multiple-timers
-        this.spawn = this.time.addEvent({delay: this.dogSpawnRate, loop: true});
+ 
 
         this.dogs = this.physics.add.group();
-        this.physics.add.collider(this.dogs, this.ground);
+        this.garbage = this.physics.add.group(); // Trash should pass through the ground
 
-        // this.physics.add.overlap(this.grandma, this.redLaser1, this.detonate, null, this);
-        // this.physics.add.overlap(this.grandma, this.purpleLaser, this.detonate, null, this);
-        // this.physics.add.overlap(this.grandma, this.yellowLaser, this.detonate, null, this);
-        // this.physics.add.overlap(this.grandma, this.flag, this.goalTouched, null, this);
+        // timer event - https://rexrainbow.github.io/phaser3-rex-notes/docs/site/timer/
+        this.dogSpawnTimer = this.time.addEvent({
+            delay: this.dogSpawnRate,
+            callback: this.spawnDog,
+            callbackScope: this,
+            loop: true
+        });
+
+        this.trashSpawnTimer = this.time.addEvent({
+            delay: this.trashSpawnRate,
+            callback: this.spawnTrash,
+            callbackScope: this,
+            loop: true
+        });
+
+        this.physics.add.overlap(this.player, this.shelter, this.returnDog, null, this);
         
         this.announcementStyle = { font: "30px Verdana", fill: "#FFFFFF", align: "center" };
 
-        //this.explosion.play();
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        // let style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        // let text = this.add.text( this.cameras.main.centerX, 15, "If you're seeing this,\nthat means I have decided to drop this assignment", style );
-        // text.setOrigin( 0.5, 0.0 );
+        this.playerPosX = this.player.x;
+        this.playerPosY = this.player.y;
     }
     
     update() {
-
-        //console.log("Event progress: " + this.spawn.getProgress().toString().substr(0,4));
+        this.playerPosX = this.player.x;
+        this.playerPosY = this.player.y;
 
         /* Movement control - https://phaser.io/tutorials/making-your-first-phaser-3-game/part7 */
         if (this.cursors.left.isDown)
@@ -114,43 +124,183 @@ class MyScene extends Phaser.Scene {
         switch(this.boxPosition)
         {
             case 1:
-                this.boxIsUp ? this.box.setPosition(this.player.x, this.player.y - 50) : this.boxUp();
+                this.boxIsUp ? this.box.setPosition(this.player.x, this.player.y - 90) : this.boxUp();
                 break;
             case 2:
-                this.box.setPosition(this.player.x + 45, this.player.y);
+                this.box.setPosition(this.player.x + 70, this.player.y);
                 this.boxDown();
                 break;
             case 3:
-                this.box.setPosition(this.player.x - 45, this.player.y);
+                this.box.setPosition(this.player.x - 70, this.player.y);
                 this.boxDown();
                 break;
         }
 
     }
 
+    // Rotates the box if it is up
     boxUp()
     {
         this.boxIsUp = true;
         this.box.angle = 90;
     }
 
+    // Rotates the box if it is down
     boxDown()
     {
         this.boxIsUp = false;
         this.box.angle = 0;
     }
 
-    collectTrash(player, trash) {
-        trash.disableBody(true, true);
-        score += 10;
-        this.scoreText = this.add.text(16, 16, `score: ${this.score}`, {fontSize: '32px', fill: '#FFFFFF'});
+    setDogCollider(dog)
+    {
+        this.physics.add.overlap(this.box, dog, this.captured_Dog, null, this);
+        this.physics.add.overlap(this.player, dog, this.hitPlayer_Dog, null, this);
+    }
+    // Spawns Dog
+    spawnDog()
+    {
+        console.log("Attempting to spawn dog, this.gameStart = " + this.gameStart);
+        if (this.gameStart)
+        {
+            console.log("HOIIIIIII");
+            let randInt = Phaser.Math.Between(1, 2);
+            console.log("Random Int for Dog: " + randInt);
+            let dog = null;
+            switch (randInt)
+            {
+                case 1:
+                    dog = this.dogs.create(0, 530, 'dog').setOrigin(0.5, 0.5).setVelocityX(200).setScale(0.45).refreshBody();
+                    dog.body.setAllowGravity(false);
+                    dog.flipX = true;
+                    break;
+                case 2:
+                    dog = this.dogs.create(900, 530, 'dog').setOrigin(0.5, 0.5).setVelocityX(-200).setScale(0.45).refreshBody();
+                    dog.body.setAllowGravity(false);
+                    break;
+            }
+            this.setDogCollider(dog);
+            this.dogs.children.iterate(function(dog) {
+                if (dog.x < -110 || dog.x > 910)
+                {
+                    dog.disableBody(true, true);
+                    console.log("Dog destroyed");
+                }
+            });
+            console.log("Dog spawned HOIIIIIIIIIIIII");
+        }
     }
 
-    captured(box, dog)
+    // Initializes colliders for trash
+    setTrashCollider(trash)
     {
+        this.physics.add.collider(trash, this.ground, this.destroyGarbage, null, this); // Destroys garbage that are out of bounds (literally garbage-handling the data)
+        this.physics.add.overlap(this.box, trash, this.collectTrash, null, this);
+        this.physics.add.overlap(this.player, this.garbage, this.hitPlayer_Trash, null, this);
+    }
+    // Spawns Trash
+    spawnTrash()
+    {
+        console.log("Attempting to spawn TRASH, this.gameStart = " + this.gameStart);
+        if (this.gameStart)
+        {
+            let randInt = Phaser.Math.Between(100, 700);
+            console.log("Random Int for Trash: " + randInt);
+            let trash = this.garbage.create(randInt, 50, 'trash').setOrigin(0.5, 0.5).setVelocityY(150).setScale(0.075);
+            this.setTrashCollider(trash);
+            this.dogs.children.iterate(function(dog) {
+                if (dog.x < -110 || dog.x > 910)
+                {
+                    dog.disableBody(true, true);
+                    console.log("Dog destroyed");
+                }
+            });
+            console.log("Trash spawned HOIIIIIIIIIIIII");
+        }
+    }
+
+    destroyGarbage(garbage, ground)
+    {
+        garbage.disableBody(true, true);
+    }
+
+    // Collect trash
+    collectTrash(player, trash) {
+        console.log("Trash collected");
+        this.confirmation.play();
+        trash.disableBody(true, true);
+        this.score += 10;
+        this.scoreText.setText('score: ' + this.score);
+    }
+
+    hitPlayer_Trash(player, trash)
+    {
+        console.log("Player is hit by trash");
+        this.oof.play();
+        this.ouch.play();
+
+        trash.disableBody(true, true);
+        this.captured = Math.floor(this.captured / 2); // Player loses half of captured dogs
+        this.lives--;
+        this.capturedText.setText('captured: ' + this.captured);
+        this.healthText.setText('HEALTH: ' + this.lives);
+
+        if (this.lives <= 0)
+        {
+            this.detonate();
+        }
+        else
+        {
+            this.oof.play();
+            this.ouch.play();
+        }
+    }
+
+    // Capture dog
+    captured_Dog(box, dog)
+    {
+        console.log("DOG CAPTURED");
+        this.squeak.play();
         dog.disableBody(true, true);
         this.captured++;
-        this.capturedText = this.capturedText = this.add.text(16, 64, `captured: ${this.score}`, {fontSize: '32px', fill: '#FFFFFF'});
+        this.capturedText.setText('captured: ' + this.captured);
+    }
+
+    // Returning the dog to the shelter will reward players with 5 times the normal point amount
+    returnDog(player, shelter)
+    {
+        if (this.captured > 0)
+        {
+            let totalPoints = 50 * this.captured;
+            this.score += totalPoints;
+            this.captured = 0;
+            this.scoreText.setText('score: ' + this.score);
+            this.capturedText.setText('captured: ' + this.captured);
+            this.ding.play();
+        }
+
+        
+    }
+
+    hitPlayer_Dog(player, dog)
+    {
+        console.log("Player is hit by dog");
+        
+        dog.disableBody(true, true);
+        this.captured = Math.floor(this.captured / 2); // Player loses half of captured dogs
+        this.lives--;
+        this.capturedText.setText('captured: ' + this.captured);
+        this.healthText.setText('HEALTH: ' + this.lives);
+
+        if (this.lives <= 0)
+        {
+            this.detonate();
+        }
+        else
+        {
+            this.oof.play();
+            this.ouch.play();
+        }
     }
 
     // Creates the background image and scales it to the size of the screen
@@ -183,34 +333,36 @@ class MyScene extends Phaser.Scene {
 
         this.W = this.input.keyboard.addKey('W');
         this.A = this.input.keyboard.addKey('A');
-        this.S = this.input.keyboard.addKey('S');
+        //this.S = this.input.keyboard.addKey('S');
         this.D = this.input.keyboard.addKey('D');
     }
 
     detonate() {
-        // console.log("BOOOOOM GRANNY DEAD, YOU FAILED");
-        // if (this.grannyAlive) {
-        //     this.explosion.play();
-        // }
-        // this.grannyAlive = false;
-        // this.grandma.disableBody(true, true);
+        console.log("BOOOOOM YOU DIED, YOU FAILED");
+        if (this.playerAlive) {
+            this.explosion.play();
+        }
+        this.playerAlive = false;
+        this.player.disableBody(true, true);
+        this.box.disableBody(true, true);
 
-        // this.explosionAnim = this.add.sprite(this.grandmaPosX - 100, this.grandmaPosY - 100, 'explosion').setOrigin(0);
-        // this.explosionAnim.setScale(3);
-        // this.anims.create({
-        //     key: 'explosion_anim',
-        //     frames: this.anims.generateFrameNumbers("explosion"),
-        //     frameRate: 10,
-        //     repeat: 0, // No repeats
-        //     hideOnComplete: true
-        // });
-        // this.explosionAnim.play('explosion_anim');
+        this.explosionAnim = this.add.sprite(this.playerPosX, this.playerPosY, 'explosion').setOrigin(0.5, 0.5);
+        this.explosionAnim.setScale(3);
+        this.anims.create({
+            key: 'explosion_anim',
+            frames: this.anims.generateFrameNumbers("explosion"),
+            frameRate: 10,
+            repeat: 0, // No repeats
+            hideOnComplete: true
+        });
+        this.explosionAnim.play('explosion_anim');
 
-        // this.bgMusic.stop();
+        this.bgMusic.stop();
 
-        // this.announcementText = this.add.text( this.cameras.main.centerX, this.cameras.main.centerY - 25, 
-        //     "Grandma exploded and it's all your fault\nGAME OVER", this.announcementStyle );
-        // this.announcementText.setOrigin(0.5, 0);
+        this.announcementText = this.add.text( this.cameras.main.centerX, this.cameras.main.centerY - 25, 
+            "You have failed\nGAME OVER", this.announcementStyle );
+        this.announcementText.setOrigin(0.5, 0);
+        this.gameStart = false;
     }
 
     // Adds image to a variable
@@ -223,13 +375,16 @@ class MyScene extends Phaser.Scene {
         this.player.body.setGravityY(300);
 
         this.box = this.physics.add.image(this.player.x, this.player.y, 'yellow').setOrigin(0.5, 0.5);
-        this.box.setScale(0.10, 0.25);
-        //this.box.setCollideWorldBounds(true);
+        this.box.setScale(0.15, 0.75);
         this.box.body.setAllowGravity(false);
 
 
         this.ground = this.physics.add.staticGroup().setOrigin(0);
         this.ground.create(600, 610, 'ground').setScale(1, 0.02).refreshBody();
+
+        // Animal shelter or somethin
+        this.shelter = this.physics.add.image(700, 520, 'black').setScale(0.025, 0.05).setOrigin(0.5, 0.5);
+        this.shelter.body.setAllowGravity(false);
 
     }
 
@@ -243,6 +398,7 @@ class MyScene extends Phaser.Scene {
         this.load.image('man', 'assets/man.png');
         this.load.image('dog', 'assets/annoyingDog.png');
         this.load.image('ground', 'assets/green.png');
+        this.load.image('trash', 'assets/trash.png');
 
         this.load.spritesheet('explosion', 'assets/explosion3.png', {
             frameWidth: 128,
@@ -255,6 +411,11 @@ class MyScene extends Phaser.Scene {
         this.load.audio('bgMusic', ['assets/326 Chiptune Cascade by Kubbi  Royalty Free Music.mp3']);
         this.load.audio('explosion', ['assets/explosion.mp3']);
         this.load.audio('beep', ['assets/beep.mp3']);
+        this.load.audio('squeak', ['assets/Squeak.mp3']);
+        this.load.audio('oof', ['assets/MinecraftOOF.mp3']);
+        this.load.audio('ouch', ['assets/ouch.mp3']);
+        this.load.audio('confirmation',['assets/UIConfirmation.mp3']);
+        this.load.audio('ding', ['assets/Ding.mp3']);
 
     }
 
@@ -263,6 +424,11 @@ class MyScene extends Phaser.Scene {
         this.bgMusic = this.sound.add('bgMusic', {volume: 0.15});
         this.explosion = this.sound.add('explosion', {volume: 0.75});
         this.beep = this.sound.add('beep', {volume: 0.75});
+        this.squeak = this.sound.add('squeak', {volume: 0.75});
+        this.oof = this.sound.add('oof', {volume: 1.0});
+        this.ouch = this.sound.add('ouch', {volume: 0.80});
+        this.confirmation = this.sound.add('confirmation', {volume: 1.15});
+        this.ding = this.sound.add('ding', {volume: 0.75});
     }
 }
 
